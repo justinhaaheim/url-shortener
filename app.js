d558cf6d4b0d2e57219a847b2f277ff5ab33d11b
Justin H <io@justinh.org>
2017-07-07 23:16:13 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// app.js
+var express = require('express');
+var app = express();
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,3 @@
+// db.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://ds151662.mlab.com:51662/jdh-rest-api'); // <dbuser>:<dbpassword>@
